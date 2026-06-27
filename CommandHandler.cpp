f5ed387d41064e17e7a3b7727b2ea34ac1e4d6d2
tahelganani <tganani2@gmail.com>
2026-06-27 18:30:20 +0300

    Create to-string helpers

--- CommandHandler.cpp
+++ CommandHandler.cpp
@@ -1,4 +1,31 @@
 #include "CommandHandler.h"
 
+#include <sstream>
+#include <unistd.h>
+
+using std::string;
+using std::ostringstream;
+
+static string currencyToString(Currency currency) {
+    if(currency == Currency::ILS) {
+        return "ILS";
+    }
+    if(currency == Currency::USD) {
+        return "USD";
+    }
+    return "";
+}
+
+static string accountDoesNotExistMessage(int ATMId, int accountId) {
+    ostringstream output;
+    output << "Error " << ATMId << ": Your transaction failed - account id " 
+        << accountId << " does not exist";
+    return output.str();
+}
+
 CommandHandler::CommandHandler(Bank& bank) : bank(bank) {}
+
+void CommandHandler::executeCommand(const Command& command, bool isRegularATM) {
+    
+}
 
