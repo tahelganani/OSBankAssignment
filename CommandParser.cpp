5ab98efeeea4d9fa94026551be214328c3b6b51f
tahelganani <tganani2@gmail.com>
2026-06-27 17:37:53 +0300

    Implement main parser functionality
    Signed-off-by: tahelganani <tganani2@gmail.com>

--- CommandParser.cpp
+++ CommandParser.cpp
@@ -22,3 +22,92 @@ void CommandParser::parseVIPIfNeeded(istringstream& input, Command& command) {
     }
 }
 
+Command CommandParser::parseLine(string& line, int sourceATMId) {
+    Command command;
+    command.sourceAtmId = sourceATMId;
+    istringstream input(line);
+    char commandType;
+    input >> commandType;
+    string currencyToken;
+
+    switch(commandType) {
+        case 'O':
+            command.type = CommandType::OPEN_ACCOUNT;
+            input >> command.accountId >> command.password
+                >> command.initialBalanceILS >> command.initialBalanceUSD;
+            break;
+
+        case 'D':
+            command.type = CommandType::DEPOSIT;
+            input >> command.accountId >> command.password
+                >> command.amount >> currencyToken;
+            command.currency = parseCurrency(currencyToken);
+            break;
+
+        case 'W':
+            command.type = CommandType::WITHDRAW;
+            input >> command.accountId >> command.password
+                >> command.amount >> currencyToken;
+            command.currency = parseCurrency(currencyToken);
+            break;
+
+        case 'B':
+            command.type = CommandType::BALANCE;
+            input >> command.accountId >> command.password;
+            break;
+
+        case 'Q':
+            command.type = CommandType::CLOSE_ACCOUNT;
+            input >> command.accountId >> command.password;
+            break;
+
+        case 'T':
+            command.type = CommandType::TRANSFER;
+            input >> command.accountId >> command.password
+                >> command.targetAccountId >> command.amount >> currencyToken;
+            command.currency = parseCurrency(currencyToken);
+            break;
+
+        case 'C':
+            command.type = CommandType::CLOSE_ATM;
+            input >> command.targetAtmId;
+            break;
+
+        case 'R':
+            command.type = CommandType::ROLLBACK;
+            input >> command.iterations;
+            break;
+
+        case 'X': {
+            command.type = CommandType::EXCHANGE;
+            string sourceCurrencyToken;
+            string toToken;
+            string targetCurrencyToken;
+            input >> command.accountId >> command.password
+                >> sourceCurrencyToken >> toToken 
+                >> targetCurrencyToken >> command.amount;
+            (void)toToken;
+            command.sourceCurrency = parseCurrency(sourceCurrencyToken);
+            command.targetCurrency = parseCurrency(targetCurrencyToken);
+            break;
+        }
+        
+        case 'I':
+            command.type = CommandType::INVEST;
+            input >> command.accountId >> command.password
+                >> command.amount >> currencyToken >> command.timeMs;
+            command.currency = parseCurrency(currencyToken);
+            break;
+
+        case 'S':
+            command.type = CommandType::SLEEP;
+            input >> command.timeMs;
+            break;
+        
+        default:
+            command.type = CommandType::INVALID;
+            break;
+    }
+    parseVIPIfNeeded(input, command);
+    return command;
+}
