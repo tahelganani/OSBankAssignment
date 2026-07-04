32a6503f0f4fc8477b37ac2e048fd927362b22b7
tahelganani <tganani2@gmail.com>
2026-07-04 11:10:42 +0300

    Implement bank functionality (excluding rollback)
    Signed-off-by: tahelganani <tganani2@gmail.com>

--- Bank.cpp
+++ Bank.cpp
@@ -2,6 +2,13 @@
 #include "ATM.h"
 #include "VIPHandler.h"
 
+#include <cstdio>
+#include <sstream>
+#include <unistd.h>
+
+using std::endl;
+using std::ostringstream;
+
 Bank::Bank() : bankAccount(0, 0, 0, 0), shouldStopBank(false) {
     pthread_mutex_init(&closeRequestsLock, nullptr);
     pthread_mutex_init(&rollbackRequestsLock, nullptr);
@@ -116,5 +123,125 @@ void Bank::runWorkers() {
     }
     stopBank();
     pthread_join(statusThread, nullptr);
+}
+
+BankSnapshot Bank::createSnapshotUnsafe() {
+    BankSnapshot bankSnapshot;
+    vector<Account*> lockedAccounts;
+    lockedAccounts.reserve(accounts.size());
+
+    for(auto it = accounts.begin() ; it != accounts.end() ; ++it) {
+        it->second->getLock().readersLock();
+        lockedAccounts.push_back(it->second);
+    }
+    bankAccount.getLock().readersLock();
+
+    for(auto it = accounts.begin() ; it != accounts.end() ; ++it) {
+        bankSnapshot.snapshots.push_back(it->second->getAccountSnapshot());
+    }
+    bankSnapshot.bankAccount = bankAccount.getAccountSnapshot();
+
+    bankAccount.getLock().readersUnlock();
+
+    for(size_t i = 0 ; i < lockedAccounts.size() ; i++) {
+        lockedAccounts[i]->getLock().readersUnlock();
+    }
+    return bankSnapshot;
+}
+
+void Bank::saveSnapshotUnsafe(const BankSnapshot& snapshot) {
+    snapshots.push_back(snapshot);
+    if(snapshots.size() > 100) {
+        snapshots.pop_front();
+    }
+}
+
+void Bank::printSnapshot(const BankSnapshot& snapshot) {
+    printf("\033[2J");
+    printf("\033[1;1H");
+    printf("Current Bank Status\n");
+    
+    for(size_t i = 0 ; i < snapshots.size() ; i++) {
+        const AccountSnapshot& account = snapshot.snapshots[i];
+
+        printf("Account %d: Balance - %d ILS %d USD, Account Password - %04d\n" ,
+            account.id, account.balanceILS, account.balanceUSD, account.password);
+    }
+    fflush(stdout);
+}
+
+void Bank::processCloseRequests() {
+    while(1) {
+        pthread_mutex_lock(&closeRequestsLock);
+        if(closeRequests.empty()) {
+            pthread_mutex_unlock(&closeRequestsLock);
+            break;
+        }
+        CloseATMRequest request = closeRequests.front();
+        closeRequests.pop();
+        pthread_mutex_unlock(&closeRequestsLock);
+
+        ATM* targetATM = getATM(request.targetATMId);
+        if(targetATM == nullptr) {
+            ostringstream output;
+            output << "Error " << request.sourceATMId <<
+                ": Your transaction failed - ATM ID " << request.targetATMId <<
+                " does not exist";
+            logger.log(output.str());
+            continue;
+        }
+        if(targetATM->isClosed()) {
+            ostringstream output;
+            output << "Error " << request.sourceATMId <<
+                ": Your close operation failed - ATM ID " << request.targetATMId <<
+                " is already in a closed state";
+            logger.log(output.str());
+            continue;
+        }
+        targetATM->requestClose();
+        ostringstream output;
+        output << "Bank: ATM " << request.sourceATMId << " closed " <<
+            request.targetATMId << " successfully";
+        logger.log(output.str());
+    }
+}
+
+void Bank::processRollbackRequests() {
+    //TO-DO
+}
+
+void Bank::statusLoop() {
+    while(!shouldStopBankRunning()) {
+        usleep(10000);
+        accountsLock.readersLock();
+
+        BankSnapshot snapshot = createSnapshotUnsafe();
+        saveSnapshotUnsafe(snapshot);
+
+        accountsLock.readersUnlock();
+
+        printSnapshot(snapshot);
+        processCloseRequests();
+        processRollbackRequests();
+    }
+}
+
+void* Bank::statusThreadEntry(void* arg) {
+    Bank* bank = static_cast<Bank*>(arg);
+    bank->statusLoop();
+    return nullptr;
+}
+
+bool Bank::shouldStopBankRunning() {
+    pthread_mutex_lock(&shouldStopBankLock);
+    bool result = shouldStopBank;
+    pthread_mutex_unlock(&shouldStopBankLock);
+    return result;
+}
+
+void Bank::stopBank() {
+    pthread_mutex_lock(&shouldStopBankLock);
+    shouldStopBank = true;
+    pthread_mutex_unlock(&shouldStopBankLock);
 }
 
